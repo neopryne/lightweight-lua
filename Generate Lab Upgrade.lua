c5b81e80a5d83d151cec1fbf39e91fb1b1baeaa8
neopryne <nope@>
2024-11-20 18:41:40 -0500

    Added multiple crew support to lab
    You can now define all your crew at once with the lab generation tool.
    You still need to define any buffs by hand.

--- Generate Lab Upgrade.lua
+++ Generate Lab Upgrade.lua
@@ -8,37 +8,66 @@ Keep a copy of this script for each of your mods, and use it whenever you want t
 --Tip: You can merge findName blocks with the exact same text if you have multiples.
 --]]
 
---Which crew types is this for?
---  Make a list of all the crew types you want this agument to apply to.  F22 doesn't have any variants, so there's just one name here.
-local CREW_TYPES = {"fff_f22"}
---What is the general term in code for this type of crew? (Upper case, no spaces)
-local CREW_NAME_INTERNAL = "FFF_F22"
---What is this type of crew called? (Spaces ok)
-local CREW_NAME = "F-22"
---The following is a list of descriptions of the augments you want to make for this crew:
-local augmentList = {
+
+local labList = {
         {
-            --What is this augment called?  (In code, this will be upper case, with spaces replaced with underscores.)
-            AUG_FRIENDLY_NAME = "Thermal Imaging",
-            --What does it do? (Short)
-            AUG_DESCRIPTION = "See crew locations even without sensors.",
-            --What does it do? (As detailed as you like.)
-            AUG_LONG_DESCRIPTION = "Your F-22 crew gain the ability to see crew even if your ship's sensors are not functioning.",
-            --How much should this upgrade cost?  (Scrap only, for other costs see cost guides.)
-            SCRAP_COST = 40
+            --  Make a list of all the crew types you want this agument to apply to.  Buffer doesn't have any variants, so there's just one name here.
+            CREW_TYPES = {"fff_buffer"},
+            --What is the general term in code for this type of crew? (Upper case, no spaces)
+            CREW_NAME_INTERNAL = "FFF_BUFFER",
+            --What is this type of crew called? (Spaces ok)
+            CREW_NAME = "Buffer",
+            --The following is a list of descriptions of the augments you want to make for this crew:
+            {
+                --What is this augment called?  (In code, this will be upper case, with spaces replaced with underscores.)
+                AUG_FRIENDLY_NAME = "Extended Memory",
+                --What does it do? (Short)
+                AUG_DESCRIPTION = "Keep track of what you were doing.",
+                --What does it do? (As detailed as you like.)
+                AUG_LONG_DESCRIPTION = "Equip your Buffers with external memory banks, allowing them to remember their stack even if they are interrupted.",
+                --How much should this upgrade cost?  (Scrap only, for other costs see cost guides.)
+                SCRAP_COST = 40
+            },
+            {
+                AUG_FRIENDLY_NAME = "Overclock",
+                AUG_DESCRIPTION = "Buffers execute commands twice as fast but have 30 less health.",
+                AUG_LONG_DESCRIPTION = "Manual tuning charges your Buffers with power, but makes their systems vulnerable. 2x execution speed, -30 health.",
+                SCRAP_COST = 50
+            }
         },
         {
-            AUG_FRIENDLY_NAME = "Supersonic Airbags",
-            AUG_DESCRIPTION = "Extra padding reduces damage to autopilots.",
-            AUG_LONG_DESCRIPTION = "Install active inflation collision devices to stop your F-22's from hurting themselves as they zoom around.  Removes the self damage from errant dashes.",
-            SCRAP_COST = 22
-        },        
-        {
-            AUG_FRIENDLY_NAME = "Freedom Boosters",
-            AUG_DESCRIPTION = "Let your F-22's excercise their god-given right to reckless endangerment in pursuit of going really fast.",
-            AUG_LONG_DESCRIPTION = "Undo the safety mechanisms on your F22's engines, increasing their speed, dash damage, stun time, and self damage by 1.3x.",
-            SCRAP_COST = 32
-        },
+            CREW_TYPES = {"fff_f22"},
+            CREW_NAME_INTERNAL = "FFF_F22",
+            CREW_NAME = "F-22",
+            {
+                --What is this augment called?  (In code, this will be upper case, with spaces replaced with underscores.)
+                AUG_FRIENDLY_NAME = "Thermal Imaging",
+                --What does it do? (Short)
+                AUG_DESCRIPTION = "See crew locations even without sensors.",
+                --What does it do? (As detailed as you like.)
+                AUG_LONG_DESCRIPTION = "Your F-22 crew gain the ability to see crew even if your ship's sensors are not functioning.",
+                --How much should this upgrade cost?  (Scrap only, for other costs see cost guides.)
+                SCRAP_COST = 40
+            },
+            {
+                AUG_FRIENDLY_NAME = "Supersonic Airbags",
+                AUG_DESCRIPTION = "Extra padding reduces damage to autopilots.",
+                AUG_LONG_DESCRIPTION = "Install active inflation collision devices to stop your F-22's from hurting themselves as they zoom around.  Removes the self damage from errant dashes.",
+                SCRAP_COST = 22
+            },        
+            {
+                AUG_FRIENDLY_NAME = "Freedom Boosters",
+                AUG_DESCRIPTION = "Let your F-22's excercise their god-given right to reckless endangerment in pursuit of going really fast.",
+                AUG_LONG_DESCRIPTION = "Undo the safety mechanisms on your F-22's engines, increasing their speed, dash damage, stun time, and self damage by 1.3x.",
+                SCRAP_COST = 32
+            },
+            {
+                AUG_FRIENDLY_NAME = "Armored Nosecones",
+                AUG_DESCRIPTION = "No door can stand in your way.",
+                AUG_LONG_DESCRIPTION = "Tiny battering rams turn your F-22's into wrecking crew, destroying any door that stands in their way.",
+                SCRAP_COST = 20
+            }
+        }
     }
 
 
@@ -57,8 +86,9 @@ local augmentList = {
 
 
 
-
-
+--This is the master version, I need to pull in F22 and make this one file.
+--todo add slug pleasure pod version.  Real easy.
+--todo add a boon + the generating code for it.
 --todo I could loop this to create several, make the current vars a table and let people add more.
 --Honestly not a bad idea, it would make the experience much easier of adding more upgrades for a single unit.
 
@@ -72,13 +102,13 @@ local blueprintText = [[
 	</blueprintList>
 ]]
 
-function storageCheckInventoryLab()
+function storageCheckInventoryLab(augmentList)
     local labInventoryChecks = ""
     for i = 1,#augmentList do
         local augment = augmentList[i]
-        local augName = string.format("LAB_%s_%s", CREW_NAME_INTERNAL, programmifyString(augment.AUG_FRIENDLY_NAME))
+        local augName = string.format("LAB_%s_%s", augmentList.CREW_NAME_INTERNAL, programmifyString(augment.AUG_FRIENDLY_NAME))
         labInventoryChecks = labInventoryChecks..string.format(STORAGE_CHECK_INVENTORY_LAB, 
-            augName, augment.AUG_FRIENDLY_NAME, CREW_NAME_INTERNAL, augName, augment.AUG_FRIENDLY_NAME)
+            augName, augment.AUG_FRIENDLY_NAME, augmentList.CREW_NAME_INTERNAL, augName, augment.AUG_FRIENDLY_NAME)
     end
     return labInventoryChecks
 end
@@ -122,28 +152,28 @@ STORAGE_CHECK_LAB_CREW_END = [[
 	</choice>
 ]]
 
-function storageCheckLabCrew()
+function storageCheckLabCrew(augmentList)
     local labCrewChecks = ""
     local labCrewChecksEnd = ""
     for i = 1,#augmentList do
         local augment = augmentList[i]
-        local augName = string.format("LAB_%s_%s", CREW_NAME_INTERNAL, programmifyString(augment.AUG_FRIENDLY_NAME))
+        local augName = string.format("LAB_%s_%s", augmentList.CREW_NAME_INTERNAL, programmifyString(augment.AUG_FRIENDLY_NAME))
         labCrewChecks = labCrewChecks..string.format(STORAGE_CHECK_LAB_CREW, 
             augName, augment.AUG_FRIENDLY_NAME, augment.AUG_DESCRIPTION)
         labCrewChecksEnd = labCrewChecksEnd..string.format(STORAGE_CHECK_LAB_CREW_END,
-            CREW_NAME_INTERNAL, i, augment.AUG_FRIENDLY_NAME, CREW_NAME_INTERNAL, i, augment.AUG_FRIENDLY_NAME, augment.SCRAP_COST, augName)
+            augmentList.CREW_NAME_INTERNAL, i, augment.AUG_FRIENDLY_NAME, augmentList.CREW_NAME_INTERNAL, i, augment.AUG_FRIENDLY_NAME, augment.SCRAP_COST, augName)
     end
     return labCrewChecks..STORAGE_CHECK_LAB_CREW_MIDDLE..labCrewChecksEnd
 end
 
-function storageCheckAugment()
+function storageCheckAugment(augmentList)
     local eventString = ""
     for i = 1,#augmentList do
         local augment = augmentList[i]
-        local augName = string.format("LAB_%s_%s", CREW_NAME_INTERNAL, programmifyString(augment.AUG_FRIENDLY_NAME))
+        local augName = string.format("LAB_%s_%s", augmentList.CREW_NAME_INTERNAL, programmifyString(augment.AUG_FRIENDLY_NAME))
         eventString = eventString..string.format(STORAGE_CHECK_AUGMENT, 
             augName, augment.AUG_FRIENDLY_NAME, augment.AUG_LONG_DESCRIPTION, CREW_NAME, augment.SCRAP_COST, augment.SCRAP_COST, augment.SCRAP_COST, augName,
-            augName, augName, CREW_NAME_INTERNAL, CREW_NAME_INTERNAL)
+            augName, augName, augmentList.CREW_NAME_INTERNAL, augmentList.CREW_NAME_INTERNAL)
     end
     return eventString
 end
@@ -182,21 +212,19 @@ STORAGE_CHECK_AUGMENT = [[
 
 ]]
 
-local specialStorageText = [[
-<!-- Begin generated lab events for %s-->
-<mod:findName type="event" name="STORAGE_CHECK_INVENTORY_LAB"> 
-%s</mod:findName>
-
-<mod:findName type="event" name="STORAGE_CHECK_LAB">
+local specialStorageHeaderEntry = [[
     <mod-append:choice req="LIST_CREW_%s" blue="false" hidden="true">
 		<text>%s.</text>
 		<event load="STORAGE_CHECK_LAB_%s"/>
     </mod-append:choice>
-    <mod:findWithChildLike type="choice" child-type="text">
-      <mod:selector>Nevermind.</mod:selector>
-      <mod:setAttributes max_group="998" req="pilot" lvl="1" blue="false"/>
-    </mod:findWithChildLike>
-</mod:findName>
+]]
+
+local specialStorageHeader = [[
+<mod:findName type="event" name="STORAGE_CHECK_LAB">%s</mod:findName>
+]]
+
+local specialStorageText = [[
+<!-- Begin generated lab events for %s-->
 
 <event name="STORAGE_CHECK_LAB_%s">
 	<text>You are viewing the lab menu for: [%s]</text>
@@ -211,61 +239,88 @@ local specialStorageText = [[
 <!-- End generated lab events for %s-->
 ]]
 
-local function getSpecialStorageText()
-    return string.format(specialStorageText,
-        CREW_NAME,
-        storageCheckInventoryLab(),
-        CREW_NAME_INTERNAL, CREW_NAME, CREW_NAME_INTERNAL,
-        CREW_NAME_INTERNAL, CREW_NAME, storageCheckLabCrew(),
-        storageCheckAugment(),
-        CREW_NAME)
+local function getSpecialStorageHeader(labList)
+    local storageEvents = ""
+    for i=1,#labList do
+        storageEvents = storageEvents..string.format(specialStorageHeaderEntry, labList[i].CREW_NAME_INTERNAL, labList[i].CREW_NAME, labList[i].CREW_NAME_INTERNAL)
+    end
+    return string.format(specialStorageHeader, storageEvents)
+end
+
+local function getSpecialStorageEntry(augmentList)
+    return string.format(specialStorageText, 
+        augmentList.CREW_NAME,
+        augmentList.CREW_NAME_INTERNAL, augmentList.CREW_NAME, storageCheckLabCrew(augmentList),
+        storageCheckAugment(augmentList),
+        augmentList.CREW_NAME)
+end
+
+local function getSpecialStorageTextInternal(labList)
+    local storageEvents = ""
+        for i=1,#labList do
+        local augmentList = labList[i]
+        storageEvents = storageEvents..getSpecialStorageEntry(augmentList)
+    end
+    return getSpecialStorageHeader(labList)..storageEvents
+end
+
+local function getSpecialStorageText(labList)
+    return getSpecialStorageTextInternal(labList)
 end
 
 local hyperspaceText = [[
-	<mod:findLike type="augments" limit="1">
-%s</mod:findLike>
+	<augments limit="1">
+%s
+    </augments>
 ]]
 
 local hyperspaceTextInternal = [[
-		<mod-append:aug name="%s">
+		<aug name="%s">
 			--You'll need to fill this out with what you want the augment to do.
-		</mod-append:aug>
+		</aug>
 ]]
 
-local function getHyperspaceTextInternal()
+local function getHyperspaceTextInternal(labList)
     local eventString = ""
-    for i = 1,#augmentList do
-        local augment = augmentList[i]
-        local augName = string.format("LAB_%s_%s", CREW_NAME_INTERNAL, programmifyString(augment.AUG_FRIENDLY_NAME))
-        eventString = eventString..string.format(hyperspaceTextInternal, 
-            augName)
+    for j =1,#labList do
+        local augmentList = labList[j]
+        for i = 1,#augmentList do
+            local augment = augmentList[i]
+            local augName = string.format("LAB_%s_%s", augmentList.CREW_NAME_INTERNAL, programmifyString(augment.AUG_FRIENDLY_NAME))
+            eventString = eventString..string.format(hyperspaceTextInternal, 
+                augName)
+        end
     end
     return eventString
 end
 
-local function blueprintNames()
+local function blueprintNames(augmentList)
     local names = ""
-    for i = 1, #CREW_TYPES do
-        names = names.."<name>"..CREW_TYPES[i].."</name>\n"
+    for i = 1, #augmentList.CREW_TYPES do
+        names = names.."<name>"..augmentList.CREW_TYPES[i].."</name>\n"
     end
     return names
 end
 
-local function getBlueprintText()
-    return string.format(blueprintText, CREW_NAME_INTERNAL, blueprintNames())
+local function getBlueprintText(labList)
+    local retString = ""
+    for i = 1,#labList do
+        retString = retString..string.format(blueprintText, labList[i].CREW_NAME_INTERNAL, blueprintNames(labList[i]))
+    end
+    return retString
 end
 
-local function getHyperspaceText()
-    return string.format(hyperspaceText, getHyperspaceTextInternal())
+local function getHyperspaceText(labList)
+    return string.format(hyperspaceText, getHyperspaceTextInternal(labList))
 end
 
-local function runProgram()
+local function runProgram(labList)
     print("\n------------------------------------------------------------------")
     print("\nPut this in autoBlueprints.xml.append:")
-    print(getBlueprintText())
+    print(getBlueprintText(labList))
     print("Put the following in your events_special_storage.xml.append:")
-    print(getSpecialStorageText())
+    print(getSpecialStorageText(labList))
     print("Finally, go put something like this in your hyperspace.xml.append.  If you have multiple augments, you can use the same findLike tag to hold all of them.")
-    print(getHyperspaceText())
+    print(getHyperspaceText(labList))
 end
-runProgram()
+runProgram(labList)
